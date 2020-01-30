ddf58beadfee59a717fb263b585ec7a347efc3b7
Dishi Jain <31739123+dishijn2@users.noreply.github.com>
2020-01-30 18:15:50 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,48 +1,26 @@
 import java.util.*;
-public class Main
-{
-	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-	    Boolean flag = false;
-		String str = sc.nextLine();
-		int len=str.length();
-		char[] ch = new char[len];
-		if(len==9){
-		for(int j=0;j<len;j++){
-		    if(j==2 || j==3 || j==4){
-		        if(Character.isLetter(str.charAt(j))) {
-		            flag = true;
-		        }
-		        else {
-		            flag = false;
-		            break;
-		        }
-		    }
-		    else if(j==0 || j==1){
-		        if(Character.isDigit(str.charAt(j))){
-		            flag = true;
-		        }
-		        else {
-		            flag = false;;
-		         }
-		    }
-		    else{
-		        if(Character.isDigit(str.charAt(j))){
-		            flag = true;
-		        }
-		        else {
-		            flag = false;
-		            break;
-		        }   
-		    }
-		}
-		}
-		else{
-		    flag=false;
-		}
-		if(flag==true)
-		    System.out.println("Valid Register Number");
-		else
-		    System.out.println("Invalid Register Number");
-	}
+class Main {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+        int size1 = s1.length();
+        int size2 = s2.length();
+        if(size1 != size2){
+            System.out.println("Not anagram");
+        }
+        else {
+            char ch1[] = s1.toCharArray();
+            char ch2[] = s2.toCharArray();
+            Arrays.sort(ch1);
+            Arrays.sort(ch2);
+            if(Arrays.equals(ch1,ch2)==true){
+                System.out.println("anagram");
+            }
+            else
+                System.out.println("not");
+        }
+    }
 }
